4345c40a383612c8d92db2f4eb8944ae4ac937c0
ctcthrn <79452448+ctcthrn@users.noreply.github.com>
2022-05-04 12:55:46 +0300

    Update code_layout.cpp

--- code/code_layout.cpp
+++ code/code_layout.cpp
@@ -21,160 +21,158 @@ enum class States
 	MOVESTRAIGHT,
 	TURN,
 	TURNSPRAYGUN,
-	PAINTING	
+	PAINTING
 };
 
-class Robot{
- 	States state;
-public: 
-  
-  void pevent(const Event& e)
-  {
-   	switch(e)
+class Robot {
+	States state;
+public:
+
+	void pevent(const Event& e)
 	{
+		switch (e)
+		{
 		case Event::PRESS_ON:
 			on();
 			state = States::ON;
 			break;
-			
+
 		case Event::PRESS_OFF:
 			off();
 			state = States::OFF;
 			break;
-			
+
 		case Event::MOVE:
 			move();
 			state = States::MOVESTRAIGHT;
 			break;
-			
+
 		case Event::TURN30:
 			turn30();
 			state = States::TURN;
 			break;
-			
+
 		case Event::TURN_SPRAY_GUN:
 			turn_spray_gun();
 			state = States::TURNSPRAYGUN;
 			break;
-				
+
 		case Event::DETECTED_GRAFFITI:
 			detected_graffiti();
 			state = States::GETTINGCOORDINATES;
 			break;
-			
+
 		case Event::START_PAINTING:
 			start_painting();
 			state = States::PAINTING;
 			break;
-			
+
 		case Event::STOP_PAINTING:
 			stop_painting();
 			state = States::OFF;
 			break;
+		}
 	}
-  }
-  
-  void on(){
-  	state = States::ON;
-  }
 
-  void off(){
-  	state = States::OFF;
-  }
-  
-  void getting_coordinated(){
-  	state = States::GETTINGCOORDINATES;
-  }
-  
-  void move(){
-  	state = States::MOVESTRAIGHT;
-  }
-  
-  void turn30(){
-  	state = States::TURN;
-  }
-  
-  void turn_spray_gun(){
-  	state = States::TURNSPRAYGUN;
-  }
-  
-  void start_painting(){
-  	state = States::PAINTING;
-  }
-  
-  void stop_painting(){
-  	state = States::OFF;
-  }
-  
-  void detected_graffiti(){
-  	state = States::GETTINGCOORDINATES;
-  }
+	void on() {
+		state = States::ON;
+	}
+
+	void off() {
+		state = States::OFF;
+	}
+
+	void getting_coordinated() {
+		state = States::GETTINGCOORDINATES;
+	}
+
+	void move() {
+		state = States::MOVESTRAIGHT;
+	}
+
+	void turn30() {
+		state = States::TURN;
+	}
+
+	void turn_spray_gun() {
+		state = States::TURNSPRAYGUN;
+	}
+
+	void start_painting() {
+		state = States::PAINTING;
+	}
+
+	void stop_painting() {
+		state = States::OFF;
+	}
+
+	void detected_graffiti() {
+		state = States::GETTINGCOORDINATES;
+	}
 };
 
-class CameraSystem{
-  States state;
+class CameraSystem {
+	States state;
 public:
-  void detected_graffiti(){
-  	state = States::GETTINGCOORDINATES;
-  } 
-  
-  void call_user(){
-  	state = States::GETTINGCOORDINATES;
-  } 
-  
-  void call_robot_coordinates(){
-  	state = States::GETTINGCOORDINATES;
-  } 
+	void detected_graffiti() {
+		state = States::GETTINGCOORDINATES;
+	}
+
+	void call_user() {
+		state = States::GETTINGCOORDINATES;
+	}
+
+	void call_robot_coordinates() {
+		state = States::GETTINGCOORDINATES;
+	}
 };
 
-class ConsoleCommand
-{
+class ConsoleCommand {
 public:
 	Event getEvent()
 	{
 		char c;
-		cin >> c;
-		
-		while (c != '0' && c != '1' && c != 'm' && c != 't' && c != 's' && c != 'd' && c != 'p' && c != 'e')
+
+		while (true)
 		{
-			cout << "ERROR. INVAILD VALUE";
 			cin >> c;
-		}
-		
-		switch(c)
-		{
+
+			switch (c)
+			{
 			case '0':
 				return Event::PRESS_OFF;
-				
+
 			case '1':
 				return Event::PRESS_ON;
-		
+
 			case 'm':
 				return Event::MOVE;
-				
+
 			case 't':
 				return Event::TURN30;
-				
+
 			case 's':
 				return Event::TURN_SPRAY_GUN;
-			
+
 			case 'd':
 				return Event::DETECTED_GRAFFITI;
-			
+
 			case 'p':
 				return Event::START_PAINTING;
-				
+
 			case 'e':
 				return Event::STOP_PAINTING;
-				
+			default:
+				cout << "incorrect";
+			}
 		}
 	}
 };
 
-int main(){
+int main() {
 	Robot r;
 	ConsoleCommand cmd;
-	
+
 	while (true)
 	{
 		Event e = cmd.getEvent();
