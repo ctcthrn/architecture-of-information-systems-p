20341eac457a1bd30522536deef5885de5b8eea1
Ladakunshina <79451500+Ladakunshina@users.noreply.github.com>
2022-06-16 14:15:48 +0300

    Update code_layout.cpp
    <3

--- code/code_layout.cpp
+++ code/code_layout.cpp
@@ -1,40 +1,63 @@
 #include <iostream>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
 using namespace std;
+
+const int MSG_SIZE = 2;
+const int SERVER_PORT = 3425;
 
 enum class Event
 {
 	PRESS_ON,
 	PRESS_OFF,
-	DETECTED_GRAFFITI,
 	MOVE,
-	TURN30,
+	TURN,
 	TURN_SPRAY_GUN,
-	START_PAINTING,
-	STOP_PAINTING
+	PAINT
 };
 
 enum class States
 {
-	ON,
 	OFF,
-	GETTINGCOORDINATES,
-	MOVESTRAIGHT,
-	TURN,
-	TURNSPRAYGUN,
+    AWAITING,
+	GETTING_COORDINATES,
+	MOVING_STRAIGHT,
+	TURNING,
+	TURNING_SPRAY_GUN,
 	PAINTING
 };
 
+class Engine
+{
+public:
+    void run();
+}
+
+class SprayGun
+{
+public:
+    void turn();
+    void spray();
+}
+
 class Robot {
 	States state;
+    const StrategyCommand* cmdrcv;
+    const Engine* eng;
+    const SprayGun* gun;
 public:
 
-	void pevent(const Event& e)
+    Robot(const StrategyCommand &cmd, const Engine &engines, const SprayGun &spg): cmdrcv(cmd), eng(engines), gun(spg) {}
+
+	void proc_event(const Event& e)
 	{
 		switch (e)
 		{
 		case Event::PRESS_ON:
 			on();
-			state = States::ON;
+			state = States::AWAITING;
 			break;
 
 		case Event::PRESS_OFF:
@@ -43,92 +66,71 @@ public:
 			break;
 
 		case Event::MOVE:
-			move();
-			state = States::MOVESTRAIGHT;
+			state = States::MOVING_STRAIGHT;
+            move();
+			state = States::AWAITING;
 			break;
 
-		case Event::TURN30:
-			turn30();
-			state = States::TURN;
+		case Event::TURN:
+			state = States::TURNING;
+            turn();
+            state = States::AWAITING;
 			break;
 
 		case Event::TURN_SPRAY_GUN:
+            state = States::TURNING_SPRAY_GUN;
 			turn_spray_gun();
-			state = States::TURNSPRAYGUN;
+			state = States::AWAITING;
 			break;
 
-		case Event::DETECTED_GRAFFITI:
-			detected_graffiti();
-			state = States::GETTINGCOORDINATES;
-			break;
-
-		case Event::START_PAINTING:
+		case Event::PAINT:
+            state = States::PAINTING;
 			start_painting();
-			state = States::PAINTING;
+			state = States::AWAITING;
 			break;
 
-		case Event::STOP_PAINTING:
-			stop_painting();
-			state = States::OFF;
-			break;
+		default:
+            cout << "Error while processing event" << endl;
 		}
 	}
 
+    void run()
+    {
+        while (true)
+            this->proc_event(cmdrcv->getEvent());
+    }
+
 	void on() {
-		state = States::ON;
+		//starting procedures
 	}
 
 	void off() {
-		state = States::OFF;
-	}
-
-	void getting_coordinated() {
-		state = States::GETTINGCOORDINATES;
+		//stopping procedures
 	}
 
 	void move() {
-		state = States::MOVESTRAIGHT;
+        eng[0].run();
+        eng[1].run();
+		//run engines
 	}
 
-	void turn30() {
-		state = States::TURN;
+	void turn() {
+        eng[0].run();
+		//run engines
 	}
 
 	void turn_spray_gun() {
-		state = States::TURNSPRAYGUN;
+        gun.turn();
+		//turn spray gun
 	}
 
-	void start_painting() {
-		state = States::PAINTING;
-	}
-
-	void stop_painting() {
-		state = States::OFF;
-	}
-
-	void detected_graffiti() {
-		state = States::GETTINGCOORDINATES;
-	}
-};
-
-class CameraSystem {
-	States state;
-public:
-	void detected_graffiti() {
-		state = States::GETTINGCOORDINATES;
-	}
-
-	void call_user() {
-		state = States::GETTINGCOORDINATES;
-	}
-
-	void call_robot_coordinates() {
-		state = States::GETTINGCOORDINATES;
+	void paint() {
+        gun.spray();
+		//paint
 	}
 };
 
 //СТРАТЕГИЯ
-
 class StrategyCommand
 {
 public:
@@ -137,11 +139,68 @@ public:
 
 class InternetCommand : public StrategyCommand
 {
+    int sock;
+    struct sockaddr_in addr;
+    char buf[MSG_SIZE+1];
+    int bytes_read;
+
 public:
+    InternetCommand()
+    {
+        sock = socket(AF_INET, SOCK_DGRAM, 0);
+        
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(SERVER_PORT);
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        this->Connect();
+    }
+
+    void Connect()
+    {
+        if(bind(sock, (struct sockaddr*)&addr, sizeof(addr))<0)
+        {
+            cout << "Error couldnt bind" << endl;
+        }
+    }
+
 	Event getEvent()
-	{
-		return Event::PRESS_OFF;
-	}
+    {
+        bytes_read = recvfrom(sock, buf, MSG_SIZE, 0, NULL, NULL);
+        buf[MSG_SIZE] = '\0';
+
+        
+        switch (buf[0])
+        {
+        case '0':
+            return Event::PRESS_OFF;
+
+        case '1':
+            return Event::PRESS_ON;
+
+        case 'm':
+            return Event::MOVE;
+
+        case 't':
+            return Event::TURN;
+
+        case 's':
+            return Event::TURN_SPRAY_GUN;
+
+        case 'p':
+            return Event::PAINT;
+
+        default:
+            cout << "incorrect" << endl;
+        }
+
+    }
+
+    ~InternetCommand()
+    {
+        delete[] buf;
+        close(sock);
+    }
 };
 
 class ConsoleCommand : public StrategyCommand
@@ -167,35 +226,25 @@ public:
 				return Event::MOVE;
 
 			case 't':
-				return Event::TURN30;
+				return Event::TURN;
 
 			case 's':
 				return Event::TURN_SPRAY_GUN;
 
-			case 'd':
-				return Event::DETECTED_GRAFFITI;
-
 			case 'p':
-				return Event::START_PAINTING;
+				return Event::PAINT;
 
-			case 'e':
-				return Event::STOP_PAINTING;
 			default:
-				cout << "incorrect";
+				cout << "incorrect" << endl;
 			}
 		}
 	}
 };
 
-int main() {
+int main()
+{
 	Robot r;
-	StrategyCommand* cmd;
-	cmd = new ConsoleCommand();
+	StrategyCommand* cmd = new ConsoleCommand();
 
-	while (true)
-	{
-		Event e = cmd->getEvent();
-		r.pevent(e);
-	}
+    r.run(cmd);
 }
-
